68036208b71f4db798116da6fb75bbec9ca1b8cb
AndreeaTuleasca <andreea.tuleasca@gmail.com>
2016-10-06 12:15:57 +0100

    Added stylig for menu bar

--- src/MatrixHeader.js
+++ src/MatrixHeader.js
@@ -17,16 +17,25 @@ export default class MatrixHeader extends React.Component{
         }
         return (<div className="matrix-header" style={divStyle}>
                     <div className="header-row">
-                        <div className="header-column header-column-wide">Score</div>
-                        <div className="header-column header-column-narrow">
-                            <div>Flags</div><div>{this.props.availableFlags}</div>
+                        <div className="header-column header-column-wide">
+                            <div className="matrix-header-text">Score</div>
+                            <div className="matrix-header-value">67</div>
                         </div>
                         <div className="header-column header-column-narrow">
-                        <img className="matrix-header-icon" src={emoticon} alt="smiley"/></div>
-                        <div className="header-column header-column-narrow">
-                            <div>Bombs</div><div>{this.props.bombs}</div>
+                            <div className="matrix-header-text">Flags</div>
+                            <div className="matrix-header-value">{this.props.availableFlags}</div>
                         </div>
-                        <div className="header-column header-column-wide">Highest score</div>
+                        <div className="header-column header-column-narrow">
+                            <img className="matrix-header-icon" src={emoticon} alt="smiley"/>
+                        </div>
+                        <div className="header-column header-column-narrow">
+                            <div className="matrix-header-text">Bombs</div>
+                            <div className="matrix-header-value">{this.props.bombs}</div>
+                        </div>
+                        <div className="header-column header-column-wide">
+                            <div className="matrix-header-text">Highest score</div>
+                            <div className="matrix-header-value">100</div>
+                        </div>
                     </div>
                 </div>)
     };
--- src/MenuBar.js
+++ src/MenuBar.js
@@ -1,23 +1,24 @@
 import React, {Component} from 'react';
+import './MenuBar.css'
 
 export default class MenuBar extends Component{
     render(){
         return (<div className="menu-bar">
                     <form onSubmit={this._handleSubmit.bind(this)}>
-                        <div className="menu-input">
-                            <span>Bombs</span>
-                            <input type="number" defaultValue={this.props.bombs} ref={(input) => this._bombs = input}/>
+                        <div className="input-container">
+                            <span className="input-label">Bombs</span>
+                            <input className="input-box" type="number" defaultValue={this.props.bombs} ref={(input) => this._bombs = input}/>
                         </div>
-                        <div className="menuInput">
-                            <span>Rows</span>
-                            <input type="number" defaultValue={this.props.rows} ref={(input) => {this._rows = input}}/>
+                        <div className="input-container">
+                            <span className="input-label">Rows</span>
+                            <input className="input-box" type="number" defaultValue={this.props.rows} ref={(input) => {this._rows = input}}/>
                         </div>
-                        <div className="menu-input">
-                            <span>Columns</span>
-                            <input type="number" defaultValue={this.props.columns} ref={(input => this._columns = input)}/>
+                        <div className="input-container">
+                            <span className="input-label">Columns</span>
+                            <input className="input-box" type="number" defaultValue={this.props.columns} ref={(input => this._columns = input)}/>
                         </div>
-                        <div>
-                            <button type="submit">Generate</button>
+                        <div className="input-container">
+                            <button className="button-submit" type="submit">Generate</button>
                         </div>
                     </form>
                 </div>);
